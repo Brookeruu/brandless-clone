264b49865365c9954c7eec84ec1926cec1484563
Brooke <perrysbrooke@gmail.com>
2019-01-31 11:33:56 -0800

    center img on item card

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,16 +1,16 @@
 import { Item } from "./item.model";
 
 export const ITEMS: Item[] = [
-  new Item("Granola Bar",
-           "Snacks",
-           3),
-  new Item("Oatmeal",
-           "Breakfast",
-           3),
-  new Item("Hazelnut Spread",
-           "Nuts & Seed Butters",
-           3),
-  new Item("Hummus Crisps",
-          "Snacks",
-           3),
+  // new Item("Granola Bar",
+  //          "Snacks",
+  //          3),
+  // new Item("Oatmeal",
+  //          "Breakfast",
+  //          3),
+  // new Item("Hazelnut Spread",
+  //          "Nuts & Seed Butters",
+  //          3),
+  // new Item("Hummus Crisps",
+  //         "Snacks",
+  //          3),
 ];
